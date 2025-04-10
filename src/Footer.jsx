c92c9214da05868edd474803426d108f5f49c59d
shivam <shivam675maur@gmail.com>
2025-04-10 22:59:07 +0530

    project setup

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+
+<footer className="footer footer-center bg-base-200 text-base-content p-4 fixed bottom-0">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
+      
+    </div>
+  )
+}
+
+export default Footer
